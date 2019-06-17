b17b95e5a1bdab4ef643940e5c9a29954a29ec4c
Albert Chu <albertchu539@gmail.com>
2019-06-16 19:45:01 -0600

    Fixed calendar menu mobile styling and added sick ass fades to details bro.

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { isMobile } from "../../hooks/breakpoints"
 import CalendarMenu from "../CalendarMenu"
+import Fade from "react-reveal/Fade"
 import "./Details.css"
 
 const Details = () => {
@@ -8,30 +9,37 @@ const Details = () => {
     <div className="Details_container">
       <div className="Details_border">
         <div className="Details_internal_container">
-          <div className="Details_internal_container_title">the wedding</div>
+          <Fade delay={100}>
+            <div className="Details_internal_container_title">the wedding</div>
+          </Fade>
+
           <div className="Details_internal_container_info Details_when">
-            <div className="Details_internal_container_header_container">
-              <span className="Details_internal_container_header">When</span>
-            </div>
-            <div>July 25 2020</div>
-            <div>ceremony: 2:00pm</div>
-            <div>reception: 6:00pm</div>
-            <div className="Details_calendarMenu">
-              <CalendarMenu />
-            </div>
-            <div />
+            <Fade left delay={200}>
+              <div className="Details_internal_container_header_container">
+                <span className="Details_internal_container_header">When</span>
+              </div>
+              <div>July 25 2020</div>
+              <div>ceremony: 2:00pm</div>
+              <div>reception: 6:00pm</div>
+              <div className="Details_calendarMenu">
+                <CalendarMenu />
+              </div>
+              <div />
+            </Fade>
           </div>
           {!isMobile() && (
             <div className="Details_internal_container_divider" />
           )}
           <div className="Details_internal_container_info Details_where">
-            <div className="Details_internal_container_header_container">
-              <span className="Details_internal_container_header">Where</span>
-            </div>
-            <div>Fairmont Palliser</div>
-            <div>133 9 Ave SW</div>
-            <div>Calgary AB</div>
-            <div>Canada T2P 2M3</div>
+            <Fade right delay={200}>
+              <div className="Details_internal_container_header_container">
+                <span className="Details_internal_container_header">Where</span>
+              </div>
+              <div>Fairmont Palliser</div>
+              <div>133 9 Ave SW</div>
+              <div>Calgary AB</div>
+              <div>Canada T2P 2M3</div>
+            </Fade>
           </div>
         </div>
       </div>
