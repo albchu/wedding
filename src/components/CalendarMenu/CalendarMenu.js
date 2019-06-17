4483e4a28832febb805c0fa303983ebd34fcaf48
Albert Chu <albertchu539@gmail.com>
2019-06-16 22:30:59 -0600

    Fixed calendar details

--- src/components/CalendarMenu/CalendarMenu.js
+++ src/components/CalendarMenu/CalendarMenu.js
@@ -3,12 +3,12 @@ import AddToCalendar from "react-add-to-calendar"
 import "./CalendarMenu.css"
 
 const calendarEvent = {
-  title: "Vicky & Albert's wedding!",
+  title: "Vicky & Albert's Wedding!",
   description:
     "Party it dup with Vicky and Albert on their friggin wedding day!",
   location: "Fairmont Palliser, 133 9 Ave SW, Calgary, AB T2P 2M3, Canada",
   startTime: "2020-07-25T14:00:00-07:00",
-  endTime: "2020-07-26T23:59:00-07:00",
+  endTime: "2020-07-25T23:59:00-07:00",
 }
 
 const items = [
