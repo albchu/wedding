ff410839a8377472979fdaed411c37db93837866
Albert Chu <albertchu539@gmail.com>
2019-06-16 23:27:50 -0600

    Basic generic framed container

--- src/components/FramedContainer/FramedContainer.js
+++ src/components/FramedContainer/FramedContainer.js
@@ -1,8 +1,21 @@
 import React from "react"
+import LogoHeader from "../LogoHeader"
 import "./FramedContainer.css"
 
-const FramedContainer = ({ children }) => {
-  return <div className="FramedContainer_container">{children}</div>
+const Blank = () => <div>hello</div>
+const Blank2 = () => <div>hello</div>
+
+const FramedContainer = ({ children, HeaderComponent = LogoHeader }) => {
+  return (
+    <div className="FramedContainer_container">
+      <div className="FramedContainer_border FramedContainer_border_top" />
+      <div className="FramedContainer_header_container">
+        <HeaderComponent />
+      </div>
+      {children}
+      <div className="FramedContainer_border FramedContainer_border_bottom" />
+    </div>
+  )
 }
 
 export default FramedContainer
---
+++ src/components/LogoHeader/LogoHeader.js
@@ -0,0 +1,13 @@
+import React from "react"
+import LogoImage from "../LogoV2"
+import "./LogoHeader.css"
+
+const LogoHeader = () => {
+  return (
+    <div className="LogoHeader_container">
+      <LogoImage />
+    </div>
+  )
+}
+
+export default LogoHeader
