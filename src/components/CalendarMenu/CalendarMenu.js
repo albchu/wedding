3d9d1ff8639dd5e3112acebef49f18e7b2349951
Albert Chu <albertchu539@gmail.com>
2019-06-16 18:27:20 -0600

    shitty basic add to calendar button

---
+++ src/components/CalendarMenu/CalendarMenu.js
@@ -0,0 +1,34 @@
+import React from "react"
+import AddToCalendar from "react-add-to-calendar"
+import "./CalendarMenu.css"
+
+const calendarEvent = {
+  title: "Vicky & Albert's wedding!",
+  description:
+    "Party it dup with Vicky and Albert on their friggin wedding day!",
+  location: "Fairmont Palliser, 133 9 Ave SW, Calgary, AB T2P 2M3, Canada",
+  startTime: "2020-07-25T14:00:00-07:00",
+  endTime: "2020-07-26T23:59:00-07:00",
+}
+
+const items = [
+  { google: "Google" },
+  { apple: "Apple Calendar" },
+  { yahoo: "Yahoo" },
+  { outlook: "Outlook" },
+  { outlookcom: "Outlook.com" },
+]
+
+const CalendarMenu = () => {
+  return (
+    <div className="CalendarMenu_container">
+      <AddToCalendar
+        event={calendarEvent}
+        buttonLabel="Add to Calendar"
+        listItems={items}
+      />
+    </div>
+  )
+}
+
+export default CalendarMenu
--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { isMobile } from "../../hooks/breakpoints"
+import CalendarMenu from "../CalendarMenu"
 import "./Details.css"
 
 const Details = () => {
@@ -9,17 +10,24 @@ const Details = () => {
         <div className="Details_internal_container">
           <div className="Details_internal_container_title">the wedding</div>
           <div className="Details_internal_container_info Details_when">
-            <span className="Details_internal_container_header">When</span>
+            <div className="Details_internal_container_header_container">
+              <span className="Details_internal_container_header">When</span>
+            </div>
             <div>July 25 2020</div>
             <div>ceremony: 2:00pm</div>
             <div>reception: 6:00pm</div>
+            <div className="Details_calendarMenu">
+              <CalendarMenu />
+            </div>
             <div />
           </div>
           {!isMobile() && (
             <div className="Details_internal_container_divider" />
           )}
           <div className="Details_internal_container_info Details_where">
-            <span className="Details_internal_container_header">Where</span>
+            <div className="Details_internal_container_header_container">
+              <span className="Details_internal_container_header">Where</span>
+            </div>
             <div>Fairmont Palliser</div>
             <div>133 9 Ave SW</div>
             <div>Calgary AB</div>
