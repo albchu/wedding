7e6f1e8e8d062df14669d46e26f6d31ed7b682e8
Albert Chu <albertchu539@gmail.com>
2019-10-30 18:40:44 -0600

    Out with the old Thanos snap.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,14 @@
 import React from "react"
 
 import SEO from "../components/seo"
+import Logo from "../components/LogoV2"
 import "./index.css"
-import Header from "../components/Header"
-import Details from "../components/Details"
-import CoffeeDate from "../components/CoffeeDate"
-import SaveTheDate from "../components/SaveTheDate"
-import { isMobile } from "../hooks/breakpoints"
 
 const Index = () => (
   <>
-    <SEO title="Save the Date" />
-    <div className="Index_container">
-      {isMobile() && <Header className="Index_header" />}
-      <div className="Index_coffeeDate">
-        <div className="Index_coffeeDate_internal">
-          <CoffeeDate />
-        </div>
-      </div>
-      <div className="Index_saveTheDateTopSpacing" />
-      <SaveTheDate className="Index_saveTheDate" />
-      <div className="Index_spacer" />
-      <Details className="Index_details" />
-      <div className="test">this is a small change i hope no one will see.</div>
-    </div>
+    <SEO title="Vicky & Albert" />
+    <Logo />
+    <div className="Index_container">we will rebuild</div>
   </>
 )
 
