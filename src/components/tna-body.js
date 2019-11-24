63dc31b47315d857d6d3f7ac4cb6b04d2b3e01a6
Albert Chu <albertchu539@gmail.com>
2019-11-23 17:15:45 -0700

    First pass at tna title and some body filler for vicky to build

---
+++ src/components/tna-body.js
@@ -0,0 +1,14 @@
+import React from "react"
+import SectionTitle from "./section-title"
+import "./tna-body.scss"
+import Ampersand from "./ampersand";
+
+const TravelAndAcommodation = () => {
+  return (
+    <div className="tna-body">
+      this is the body. Wow... lOook at it
+    </div>
+  )
+}
+
+export default TravelAndAcommodation
