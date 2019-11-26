fcb2918acc97b2d7cc6bfe72340bb6628f5f9ddf
vickylao <vickywklao@gmail.com>
2019-11-25 21:28:27 -0700

    Aligned menu body to the left :D

--- src/components/menu-body.js
+++ src/components/menu-body.js
@@ -6,9 +6,9 @@ import Ampersand from "./ampersand"
 const MenuBody = () => {
   return (
     <div className="menu_body">
-      <div className="menu_body_title">~ Soup ~</div>
-      <div className="menu_body_title">~ Salad ~</div>
-      <div className="menu_body_title">~ Entree ~</div>
+      <div className="menu_body_title">Soup</div>
+      <div className="menu_body_title">Salad</div>
+      <div className="menu_body_title">Entree</div>
       <div className="menu_body_select entree_sub">Your choice of:</div>
       <div />
       Filet Mignon
@@ -16,7 +16,7 @@ const MenuBody = () => {
       Lamb Rack
       <div />
       Salmon
-      <div className="menu_body_title">~ Dessert ~</div>
+      <div className="menu_body_title">Dessert</div>
       <p></p>
       <div className="menu_body_select">
         You will be able to select your entree and let us know any dietary
