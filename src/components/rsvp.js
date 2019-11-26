00eb90fcf411167c7662a1280e207b3a41f8b15e
Albert <albertchu539@gmail.com>
2019-11-26 12:05:38 -0700

    Added snow to rsvp page

--- src/components/rsvp.js
+++ src/components/rsvp.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react"
 import "./rsvp.scss"
-import SectionTitle from './section-title';
+import SectionTitle from "./section-title"
+import { Particles } from "@blackbox-vision/react-particles"
 
-const RSVP = ({ }) => {
+const RSVP = ({}) => {
   return (
-    <div className="rsvp" >
-      <SectionTitle title="RSVP" />
+    <div className="rsvp">
+      <div className="rsvp_centered">
+        <div className="rsvp_title">
+          <SectionTitle title="RSVP" />
+        </div>
+      </div>
+      <div className="rsvp_centered">
+        <Particles
+          id="snow"
+          width="auto"
+          height="100vh"
+          params={{
+            particles: {
+              number: {
+                value: 80,
+                density: {
+                  enable: false,
+                },
+              },
+              size: {
+                value: 2,
+                random: true,
+              },
+              move: {
+                direction: "bottom",
+                out_mode: "out",
+                speed: 1,
+              },
+              line_linked: {
+                enable: false,
+              },
+            },
+          }}
+        />
+      </div>
     </div>
   )
 }
