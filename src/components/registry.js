2e46e1d13f34d38c70c9b2ae03787ebca3faa075
vickylao <vickywklao@gmail.com>
2019-12-25 20:30:12 -0700

    update tna text and registry text :D

--- src/components/registry.js
+++ src/components/registry.js
@@ -6,6 +6,14 @@ const Registry = ({ }) => {
   return (
     <div className="registry" >
       <SectionTitle title="Registry" />
+      <div className="registry-body">
+        <p>
+          We've lived together a year or two, we really don't need anything new. 
+          </p>
+          <p>
+            If you are thinking of a giving a gift, to help us on the way, a gift of cash towards our honeymoon would really make our day.
+          </p>
+      </div>
     </div>
   )
 }
