60936a42e18a48d690f85fa9301f18220ef7a606
vickylao <vickywklao@gmail.com>
2019-12-25 21:12:51 -0700

    updated font :)

--- src/components/tna-body.js
+++ src/components/tna-body.js
@@ -15,6 +15,7 @@ const TravelAndAcommodation = () => {
       <div className="tna-Text">
         <p>We suggest you come to Calgary by flight and land at the Calgary Int'l Airport (YYC). </p>
       </div>
+      <div className="tna-padding"></div>
       <div className="hotel-Title">
         <div className="hotel-Circle"/>
         <div className="tna-Title">
@@ -23,7 +24,7 @@ const TravelAndAcommodation = () => {
       </div>
       <div className="tna-Text">
         <p>For your comfort and convenience, we have made an arrangement with the hotel to reserve a limited number of rooms at a special rate for this occasion. These rates are available for rooms staying from July 24th to 26th by mentioning the LAO-CHU wedding. For more information and room booking, please visit our link below: </p>
-        <br />
+        <div className="tna-padding"></div>
         <a href={"https://book.passkey.com/go/LAOCHU0230"}><div className="tna-Link2">Click Here</div></a>
       </div>
       <div className="sightsee-Title">
@@ -35,7 +36,7 @@ const TravelAndAcommodation = () => {
       <div className="tna-Text">
         <p>We highly recommend you visiting Banff while you are in Calgary and we have put together a sample itinerary for your visit with hotel suggestions and car rental discount code: 
         </p>
-        <br />
+        <div className="tna-padding"></div>
         <a href={"https://docs.google.com/spreadsheets/d/1vZ_-HG4DxodcUXp7cW7415PqLHFW9Zt4iUI2e_1NLbU/edit?usp=sharing"}><div className="tna-Link2">Click Here</div></a>
     </div>
     </div>
