67752aabcecab6fdc44a0f8b33ce1486338e25a7
Albert Chu <albertchu539@gmail.com>
2019-12-27 01:53:00 -0700

    Removed react reveal

--- src/components/spash-page.js
+++ src/components/spash-page.js
@@ -1,18 +1,20 @@
-import React from "react"
-import Fade from "react-reveal/Fade"
-import Logo from "./logo"
-import "./spash-page.scss"
+import React, { useState, useEffect } from "react";
+import Logo from "./logo";
+import "./spash-page.scss";
 
 const SplashPage = () => {
+  const [className, setClassName] = useState("");
+  useEffect(() => {
+    setTimeout(() => setClassName("fadeIn"), 800);
+  }, []);
+
   return (
     <div className="splashPage">
-      <div className="splashPage__logo">
-        <Fade delay={300}>
-          <Logo color={"white"} />
-        </Fade>
+      <div className={`splashPage__logo ${className}`}>
+        <Logo color={"white"} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SplashPage
+export default SplashPage;
